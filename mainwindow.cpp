96523a141bf56b384f9afd1a4a21a019793bc8e7
aklakina <54154570+aklakina@users.noreply.github.com>
2021-06-08 16:19:30 +0200

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    scene = new QGraphicsScene();
+    ui->graphicsView->setScene(scene);
+    scene->setSceneRect(ui->graphicsView->rect());
+    scene->setBackgroundBrush(QBrush(Qt::white));
+    ui->graphicsView->setSceneRect(scene->sceneRect());
+    ui->graphicsView->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+    ui->graphicsView->setScene(scene);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +21,41 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::hang(float freq) {
+    double fele=ui->graphicsView->height()/2;
+    scene->addLine(0,fele,ui->graphicsView->width(),fele,QPen(Qt::black));
+    double freq_lenth=std::max(guitar_freq[0],freq);
+    double graph_length=(double)(ui->graphicsView->width())*0.80;
+
+    if (freq_lenth>500) {
+        graph_length=(double)(ui->graphicsView->width());
+        scene->addLine(ui->graphicsView->width()/2,ui->graphicsView->height(),ui->graphicsView->width()/2,0,QPen(Qt::red));
+        int suruseg=50;
+        for (auto i=0;i<suruseg;i++) {
+            double x=graph_length/suruseg*i;
+            if (i!=50) {
+                scene->addLine(x,fele+15,x,fele-15,QPen(Qt::black));
+            }
+            text=new QGraphicsTextItem();
+            text->setPos(x-10,fele-5+(25*pow(-1,i)));
+            text->setPlainText(QString::number(freq-suruseg/2+i)+"Hz");
+            text->setScale(0.8);
+            scene->addItem(text);
+        }
+    } else {
+        double x_1=graph_length/freq_lenth*freq+(double)(ui->graphicsView->width())*0.1;
+        scene->addLine(x_1,ui->graphicsView->height(),x_1,0,QPen(Qt::red));
+        for (auto i:guitar_freq) {
+            double x=graph_length/freq_lenth*i+(double)(ui->graphicsView->width())*0.1;
+            scene->addLine(x,ui->graphicsView->height(),x,0,QPen(Qt::black));
+        }
+    }
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    scene->clear();
+    hang(ui->doubleSpinBox->value());
+    /*scene->addLine(0,ui->graphicsView->height()/2,ui->graphicsView->width(),ui->graphicsView->height()/2,QPen(Qt::black));
+    scene->addRect(100,100,50,50,QPen(Qt::black));*/
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,12 @@
-#ifndef MAINWINDOW_H
+﻿#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <qgraphicsscene.h>
+#include <QGraphicsTextItem>
+#include <QDebug>
+#include <QString>
+#include <math.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +20,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void hang(float freq);
+
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QGraphicsScene *scene;
+    QGraphicsTextItem *text;
+    const float guitar_freq[6]={
+        329.63,
+        246.94,
+        196,
+        146.83,
+        110.0,
+        82.41
+    };
 };
 #endif // MAINWINDOW_H
